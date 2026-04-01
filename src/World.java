7b5e4117d50009a4632a2fce9fd810d422ca5469
Park, Seungkeun <korea20030928@gmail.com>
2026-04-01 10:55:04 +0900

    five

--- src/World.java
+++ src/World.java
@@ -3,5 +3,9 @@ public class World {
     public static void run() {
         System.out.println("202314078");
     }
-    
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
+
 }
